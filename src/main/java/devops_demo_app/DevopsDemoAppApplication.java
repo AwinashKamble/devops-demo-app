4f6e569931b616cb4fc22a266cee7f8c581b33cd
Awinash Kamble <awinashkamble8@gmail.com>
2026-07-18 15:43:17 +0530

    added mvn files

---
+++ src/main/java/devops_demo_app/DevopsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package devops_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoAppApplication.class, args);
+	}
+
+}
