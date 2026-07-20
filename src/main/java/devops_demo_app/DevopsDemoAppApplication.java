b9e589a2ad2a334ac18a64aa24fbe5de4c62dc51
Awinash Kamble <awinashkamble8@gmail.com>
2026-07-20 20:25:14 +0530

    addded api details

--- src/main/java/devops_demo_app/DevopsDemoAppApplication.java
+++ src/main/java/devops_demo_app/DevopsDemoAppApplication.java
@@ -2,12 +2,19 @@ package devops_demo_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsDemoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsDemoAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoAppApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Welcome to DevOps CI/CD Pipeline! Built by Awinash Kamble";
+    }
 }
